cfc1f6288335e9116afd70fa811110683899e2fd
Мирослав Ананич <114872473+MiroslaV3000@users.noreply.github.com>
2023-11-08 17:06:17 +0300

    Delete Class directory

--- Class/Engineer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Worker.h"
-class Engineer : public Worker
-{
-public:
-	 bool work() override;
-	 Engineer(std::string name, std::string salary, std::string age, std::string  personalNumber);
-};
-
